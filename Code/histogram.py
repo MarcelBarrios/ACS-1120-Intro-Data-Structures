325d108216795116c7af09db89e8fe9abed9945a
Marcel2 <marcel2@Marcels-MacBook-Pro.local>
2025-02-03 00:15:23 -0400

    histogram done

---
+++ Code/histogram.py
@@ -0,0 +1,53 @@
+import re
+from collections import defaultdict
+
+
+def histogram(text):
+    words = re.findall(r'\b\w+\b', text.lower()
+                       )
+    histogram = defaultdict(int)
+    for word in words:
+        histogram[word] += 1
+    return dict(histogram)
+
+
+def unique_words(histogram):
+    return len(histogram)
+
+
+def frequency(word, histogram):
+    return histogram.get(word.lower(), 0)
+
+
+def save_histogram(histogram, filename="histogram.txt"):
+    hist_items = list(histogram.items())
+
+    def get_count(item):
+        return item[1]
+
+    hist_items.sort(reverse=True, key=get_count)
+
+    with open(filename, "w", encoding="utf-8") as file:
+        for word, count in hist_items:
+            file.write(f"{word} {count}\n")
+
+
+if __name__ == "__main__":
+    try:
+        with open("wealth_of_nations.txt", "r", encoding="utf-8") as file:
+            text = file.read()
+
+        histogram = histogram(text)
+
+        save_histogram(histogram)
+        print("Histogram saved to 'histogram.txt'.")
+
+        print("Histogram:", histogram)
+        print("Unique words:", unique_words(histogram))
+        print("Frequency of 'wealth':", frequency("wealth", histogram))
+        print("Frequency of 'nations':", frequency("nations", histogram))
+        print("Frequency of 'economy':", frequency(
+            "economy", histogram))
+
+    except FileNotFoundError:
+        print("Error: File 'Wealth_of_Nations.txt' not found. Make sure it exists in the same directory.")
